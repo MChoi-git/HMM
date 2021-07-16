4e00f8e01174b1fcd0f9f32f3b7a2af5faf89e9a
Matt Choi <mattchoi427@gmail.com>
2021-07-16 03:50:54 -0400

    started the forward computation

--- forwardbackward.py
+++ forwardbackward.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 
+# IMPORTANT: The alpha is not a matix, but a 3D TENSOR, JxMxD
 def forward(emit_matrix, trans_matrix, prior_matrix, sentences):
     """ Does the forward computation, which creates the alpha matrix
 
@@ -10,10 +11,22 @@ def forward(emit_matrix, trans_matrix, prior_matrix, sentences):
     :param prior_matrix:
     :return:
     """
-    longest_sentence = []
-    for sentence in sentences:
-        if len(sentence) > len(longest_sentence):
-            longest_sentence = sentence
-    longest_sentence = len(longest_sentence)
-    alpha_matrix = np.zeros((longest_sentence, prior_matrix.size))
-    print(alpha_matrix.shape)
+    # longest_sentence = []
+    # for sentence in sentences:
+    #     if len(sentence) > len(longest_sentence):
+    #         longest_sentence = sentence
+    # longest_sentence = len(longest_sentence)
+    # alpha_matrix = np.zeros((longest_sentence, prior_matrix.size))
+    # Calculate all alpha_1 for each tag
+    alpha_prior = emit_matrix[:, 0] * prior_matrix
+    alpha_prior /= alpha_prior.sum(axis=0, keepdims=True)
+    alpha_previous = alpha_prior
+    alpha_matrix = np.zeros((len(emit_matrix[0]) - 1, len(emit_matrix)))
+    # Calculate the summation term in alpha for t > 1
+    for x_n in range(len(emit_matrix[0]) - 1):
+        for tag_n in range(len(trans_matrix)):
+            alpha_matrix[tag_n][x_n] = emit_matrix[tag_n][x_n + 1] * (alpha_previous * trans_matrix[tag_n]).sum()
+        alpha_matrix[:, x_n] /= alpha_matrix[:, x_n].sum()
+        alpha_previous = alpha_matrix[:, x_n]
+    print(alpha_matrix)
+    return
